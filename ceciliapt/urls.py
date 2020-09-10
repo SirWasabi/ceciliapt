e6b60c4c7f3c39fe4f75bb924b816da5eeaa23d7
SirWasabi <vpereira95@gmail.com>
2020-09-10 20:39:14 +0100

    v1.2

--- ceciliapt/urls.py
+++ ceciliapt/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index
+from mainapp.views import index, mail
 
 urlpatterns = [
     path('', index, name='index'),
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.core.mail import send_mail
+from django.http import HttpResponse
 from django.shortcuts import render
 from ceciliapt.forms import MessageForm
 
@@ -11,14 +12,21 @@ def index(request, *args, **kwargs):
         mail(request)
         return render(request, "thanks.html", {})
     else:
-        return render(request, "index.html", {})
+        print(request.LANGUAGE_CODE)
+        if request.LANGUAGE_CODE == 'pt':
+            return render(request, "index-pt.html", {})
+        else:
+            return render(request, "index-en.html", {})
 
 
 def mail(request):
     if request.method == "POST":
+        print(request.POST)
         form = MessageForm(request.POST)
         if form.is_valid():
             # SETUP COM O EMAIL DA CECILIA TODO
             message = request.POST['name'] + ' ' + '(' + request.POST['email'] + ') diz: ' + request.POST['message']
             send_mail('Mensagem de ' + request.POST['name'], message, 'ceciliaptwebsite@gmail.com',
                       [request.POST['email']])
+    else:
+        return HttpResponse("<header>Unavaiable</header>")
